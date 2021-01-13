12e59a20ab72fc079231e4263d1bcd472b4fe145
Rahal-Kanishka <rahalncm@gmail.com>
2021-01-14 00:48:57 +0530

    user login implemented

--- user.js
+++ user.js
@@ -9,25 +9,6 @@ var router = express.Router();
 // create application/json parser
 var jsonParser = bodyParser.json()
 
-
-// driver model 
-/* var userSchema = mongoose.Schema({
-	_id: String,
-	name: String,
-	username: String,
-	password: String,
-	contact: String,
-	address: String,
-	role_id: String,
-	bookings: [{
-		bookingId: String,
-		seats: String,
-		price: String,
-		scheduleId: String
-	}]
-}, { collection: 'user' });
-var User = mongoose.model("User", userSchema); */
-
 // get user by name
 router.get('/:name', function (req, res) {
 	console.log('name: ', req.params.name);
@@ -98,6 +79,49 @@ router.post('/register', jsonParser, function (req, res) {
 		});
 
 	}
+});
+
+router.post('/login', jsonParser, function (req, res) {
+	const { userName, password } = req.body;
+
+	let errors = [];
+
+	if (!userName || !password) {
+		errors.push({ msg: 'Username or password can not be empty' });
+		res.status(400).send({
+			result: false,
+			data: errors
+		});
+	} else {
+		// check username exits
+		User.findOne({ userName: userName }, function (err, userObj) {
+			if (userObj) {
+				// hashing password
+				bcrypt.compare(password,userObj.password,(err,isMatch)=>{
+					if(err) throw err;
+					if(isMatch){
+						res.status(200).send({
+							result: true,
+							data: userObj
+						});
+					} else {
+						errors.push({ msg: 'Invalid Username or password' });
+							res.status(400).send({
+								result: true,
+								data: errors
+							});
+					}
+				});
+			} else {
+				errors.push({ msg: 'Invalid username or password' });
+				res.status(400).send({
+					result: false,
+					data: errors
+				});
+			}
+		});
+	}
+
 });
 
 
