64a9642fa7ea5c07f5d478a2c4a630bfaf7dce73
Rahal-Kanishka <rahalncm@gmail.com>
2021-01-23 15:35:13 +0530

    cloud db url added to configurations

--- config/configurations.js
+++ config/configurations.js
@@ -1,3 +1,4 @@
 module.exports = {
-    JWTKey: 'secretkey'
+    JWTKey: 'secretkey',
+    dbURI: 'mongodb+srv://rahal:123@transport-db-cluster.5mlal.mongodb.net/transport_db?retryWrites=true&w=majority'
 }
